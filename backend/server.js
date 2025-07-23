6d208b7c6da55026fc47353c9b850e2767b71e6c
Toufiqul Islam Anim <toufiq9610@gmail.com>
2025-07-23 15:04:33 +0600

    backend folder structure created

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
