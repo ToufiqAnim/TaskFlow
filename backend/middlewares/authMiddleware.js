31bbe2dee3d8fb800a636f6fe297c5a076a0cdce
Toufiqul Islam Anim <toufiq9610@gmail.com>
2025-07-25 16:15:04 +0600

    authmiddleware created

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import { jtw } from "jsonwebtoken";
+import User from "../models/User";
+
+const auth = async (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    if (token && token.startsWith("Bearer")) {
+      token = token.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } else {
+      res.status(401).json({ message: "Not authorized, no token found" });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Token fialed", error: error.message });
+  }
+};
+
+const adminAuth = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(401).json({ message: "Access denied, admin only" });
+  }
+};
+export const AuthMiddleware = { auth, adminAuth };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,14 @@
 import e from "express";
 import express from "express";
 import { AuthController } from "../controllers/authControllers";
+import { AuthMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 //Routes
 router.post("/signup", AuthController.signupUser);
 router.post("/login", AuthController.loginUser);
-router.get("/profile", protect, AuthController.getuserProfile);
-router.put("/profile", protect, AuthController.updateuserProfile);
+router.get("/profile", AuthMiddleware.auth, AuthController.getuserProfile);
+router.put("/profile", AuthMiddleware.auth, AuthController.updateuserProfile);
 
 export default router;
